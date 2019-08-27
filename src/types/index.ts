2747561f1338235f76027ed908a518653232b3e9
monpro <monproshen@gmail.com>
2019-08-27 21:28:05 +1000

    add axios type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 export interface AxiosRequestConfig {
-  url: string
+  url?: string
   timeout?: number
   method?: Method
   data?: any
@@ -41,4 +41,26 @@ export interface AxiosError extends Error {
   request?: any
   response?: AxiosResponse
   isAxiosError: boolean
+}
+
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise
 }
