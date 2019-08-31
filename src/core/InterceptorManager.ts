1a9c863c3d53405715a2a1c2055864dad213519b
monpro <monproshen@gmail.com>
2019-08-31 16:58:06 +1000

    add interceptor implementation

---
+++ src/core/InterceptorManager.ts
@@ -0,0 +1,36 @@
+import { ResolvedFN, RejectedFN } from '../types/index'
+
+interface Interceptor<T> {
+  resolved: ResolvedFN<T>
+  rejected?: RejectedFN
+}
+
+export default class InterceptorManager<T> {
+  private interceptors: Array<Interceptor<T> | null>
+  constructor() {
+    this.interceptors = []
+  }
+
+  use(resolved: ResolvedFN<T>, rejected?: RejectedFN): number {
+    this.interceptors.push({
+      resolved,
+      rejected
+    })
+
+    return this.interceptors.length - 1
+  }
+
+  forEach(fn: (interceptor: Interceptor<T>) => void): void {
+    this.interceptors.forEach(interceptor => {
+      if (interceptor !== null) {
+        fn(interceptor)
+      }
+    })
+  }
+
+  eject(id: number): void {
+    if (this.interceptors[id]) {
+      this.interceptors[id] = null
+    }
+  }
+}
