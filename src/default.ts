38209df5d1b642c419e8741872a44889f6cf93af
monpro <monproshen@gmail.com>
2019-09-01 14:39:19 +1000

    add defaults impl

---
+++ src/default.ts
@@ -0,0 +1,17 @@
+import { AxiosRequestConfig } from './types/index'
+
+const defaults: AxiosRequestConfig = {
+  method: 'get',
+  timeout: 0,
+  headers: {
+    common: {
+      Accept: 'application/json, text/plain, */*'
+    }
+  }
+}
+
+const methodsNoData = ['delete', 'get', 'head', 'options']
+
+methodsNoData.forEach(method => {
+  defaults.headers[method] = {}
+})
