8a86d04f40f0cd2f20758162977f9a6fee3cf3c6
monpro <monproshen@gmail.com>
2019-09-05 23:38:23 +1000

    add transform example

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,5 +1,6 @@
 import axios from '../../src/index'
 import qs from 'qs'
+import { AxiosTransformer } from '../../src/types';
 
 axios.defaults.headers.common['test2'] = 123
 
@@ -11,6 +12,25 @@ axios({
   }),
   headers: {
     test: '321'
+  }
+}).then((res) => {
+  console.log(res.data)
+})
+
+axios({
+  transformRequest: [(function(data) {
+    return qs.stringify(data)
+  }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+  transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
+    if (typeof data === 'object') {
+      data.b = 2
+    }
+    return data
+  }],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
   }
 }).then((res) => {
   console.log(res.data)
--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -4,6 +4,7 @@ import xhr from './xhr'
 import { buildUrl } from '../helpers/url'
 import { transformRequest, transformResponse } from '../helpers/data'
 import { processHeaders, flattenHeaders } from '../helpers/headers'
+import transform from './transform'
 
 export default function dispatchRequest(
   config: AxiosRequestConfig
@@ -17,7 +18,7 @@ export default function dispatchRequest(
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
   config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
+  config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
 
@@ -36,6 +37,6 @@ function transformHeaders(config: AxiosRequestConfig) {
 }
 
 function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
+  res.data = transform(res.data, res.headers, res.config.transformResponse)
   return res
 }
