ceb19ab4450aac33d45b192e2e2555023bbe37bd
monpro <monproshen@gmail.com>
2019-09-23 21:18:25 +1000

    refactor dispatchRequest func

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -2,8 +2,7 @@ import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types'
 import xhr from './xhr'
 
 import { buildUrl } from '../helpers/url'
-import { transformRequest, transformResponse } from '../helpers/data'
-import { processHeaders, flattenHeaders } from '../helpers/headers'
+import { flattenHeaders } from '../helpers/headers'
 import transform from './transform'
 
 export default function dispatchRequest(
@@ -17,7 +16,6 @@ export default function dispatchRequest(
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
-  config.headers = transformHeaders(config)
   config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
@@ -25,15 +23,6 @@ function processConfig(config: AxiosRequestConfig): void {
 function transformURL(config: AxiosRequestConfig): string {
   const { url = '', params } = config
   return buildUrl(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
 }
 
 function transformResponseData(res: AxiosResponse): AxiosResponse {
