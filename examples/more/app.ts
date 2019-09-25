91d01433967af314b972ee8d4fb447f0c5eb38fc
monpro <monproshen@gmail.com>
2019-09-25 23:34:57 +1000

    update the axios functionality and design -> fix bugs

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -1,5 +1,6 @@
 import axios from '../../src/index'
 import { AxiosError } from '../../src/helpers/error';
+import qs from 'qs';
 
 document.cookie = 'a=b'
 
@@ -13,39 +14,71 @@ document.cookie = 'a=b'
 //   console.log(res)
 // })
 
+// const instance = axios.create({
+//   xsrfCookieName: 'XSRF-TOKEN-D',
+//   xsrfHeaderName: 'X-XSRF-TOKEN-D'
+// })
+
+// instance.get('/more/get').then(res => {
+//   console.log(res)
+// })
+
+// axios.post('/more/post', {
+//   a: 1
+// }, {
+//   auth: {
+//     username: 'Yee',
+//     password: '123456'
+//   }
+// }).then(res => {
+//   console.log(res)
+// })
+
+// axios.get('/more/304').then(res => {
+//   console.log(res)
+//   console.log(res.status)
+// }).catch((e: AxiosError) => {
+//   console.log(e.message)
+// })
+
+// axios.get('/more/304', {
+//   validateStatus(status) {
+//     return status >= 200 && status < 250
+//   }
+// }).then(res => {
+//   console.log(res)
+// }).catch((e: AxiosError) => {
+//   console.log(e.message)
+// })
+
+axios.get('/more/get', {
+  params: new URLSearchParams('a=b&c=d')
+}).then(res => {
+  console.log(res)
+})
+
+axios.get('/more/get', {
+  params: {
+    a: 1,
+    b: 2,
+    c: ['a', 'b', 'c']
+  }
+}).then(res => {
+  console.log(res)
+})
+
 const instance = axios.create({
-  xsrfCookieName: 'XSRF-TOKEN-D',
-  xsrfHeaderName: 'X-XSRF-TOKEN-D'
+  paramsSerializer(params) {
+    return qs.stringify(params, { arrayFormat: 'brackets' })
+  }
 })
 
-instance.get('/more/get').then(res => {
-  console.log(res)
-})
-
-axios.post('/more/post', {
-  a: 1
-}, {
-  auth: {
-    username: 'Yee',
-    password: '123456'
+instance.get('/more/get', {
+  params: {
+    a: 1,
+    b: 2,
+    c: ['a', 'b', 'c']
   }
 }).then(res => {
   console.log(res)
-})
-
-axios.get('/more/304').then(res => {
-  console.log(res)
-  console.log(res.status)
-}).catch((e: AxiosError) => {
-  console.log(e.message)
-})
-
-axios.get('/more/304', {
-  validateStatus(status) {
-    return status >= 200 && status < 250
-  }
-}).then(res => {
-  console.log(res)
-}).catch((e: AxiosError) => {
-  console.log(e.message)
 })
