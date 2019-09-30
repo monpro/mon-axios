04f9296dd329ceea48342facb9c7c9f56b024155
monpro <monproshen@gmail.com>
2019-09-30 21:52:33 +1000

    use jasmine feature to run async test

--- test/requests.spec.ts
+++ test/requests.spec.ts
@@ -1,2 +1,36 @@
 import axios, { AxiosResponse, AxiosError } from '../src/app'
 import { getAjaxRequest } from './helper'
+describe('requests', () => {
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+
+  test('should treat single string arg as url', () => {
+    axios('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+      expect(request.method).toBe('GET')
+    })
+  })
+
+  test('should treat method value as lowercase string', done => {
+    axios({
+      url: '/foo',
+      method: 'POST'
+    }).then(response => {
+      expect(response.config.method).toBe('post')
+      done()
+    })
+
+    return getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200
+      })
+    })
+  })
+})
