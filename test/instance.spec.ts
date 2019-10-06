75dc6ab0790ec2c7f8de27af607420ee707bf861
monpro <monproshen@gmail.com>
2019-10-06 14:10:32 +1100

    finish unit tests for axios instance

--- test/instance.spec.ts
+++ test/instance.spec.ts
@@ -80,4 +80,66 @@ describe('instance', () => {
       expect(request.method).toBe('HEAD')
     })
   })
+
+  test('should use instance options', () => {
+    const instance = axios.create({ timeout: 1000 })
+
+    instance.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.timeout).toBe(1000)
+    })
+  })
+
+  test('should have defaults.headers', () => {
+    const instance = axios.create({ baseURL: 'https://api.example.com' })
+
+    expect(typeof instance.defaults.headers).toBe('object')
+    expect(typeof instance.defaults.headers.common).toBe('object')
+  })
+  test('should get the computed uri', () => {
+    const fakeConfig: AxiosRequestConfig = {
+      baseURL: 'https://www.google.com/',
+      url: '/user/12345',
+      params: {
+        idClient: 1,
+        idTest: 2,
+        testString: 'thisIsATest'
+      }
+    }
+    expect(axios.getUri(fakeConfig)).toBe(
+      'https://www.google.com/user/12345?idClient=1&idTest=2&testString=thisIsATest'
+    )
+  })
+
+  test('should have interceptors on the instance', done => {
+    axios.interceptors.request.use(config => {
+      config.timeout = 2000
+      return config
+    })
+
+    const instance = axios.create()
+
+    instance.interceptors.request.use(config => {
+      config.withCredentials = true
+      return config
+    })
+
+    let response: AxiosResponse
+    instance.get('/foo').then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200
+      })
+
+      setTimeout(() => {
+        expect(response.config.timeout).toEqual(0)
+        expect(response.config.withCredentials).toEqual(true)
+        done()
+      }, 100)
+    })
+  })
 })
