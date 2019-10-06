9de0f53180f029209b2cdcfd84e23841f00536cd
monpro <monproshen@gmail.com>
2019-10-06 18:44:08 +1100

    add interceptors test for request & response object

--- test/interceptor.spec.ts
+++ test/interceptor.spec.ts
@@ -64,4 +64,92 @@ describe('interceptors', () => {
       })
     }, 100)
   })
+
+  test('should add multiple request interceptors', () => {
+    const instance = axios.create()
+
+    instance.interceptors.request.use(config => {
+      config.headers.test1 = '1'
+      return config
+    })
+    instance.interceptors.request.use(config => {
+      config.headers.test2 = '2'
+      return config
+    })
+    instance.interceptors.request.use(config => {
+      config.headers.test3 = '3'
+      return config
+    })
+
+    instance('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders.test1).toBe('1')
+      expect(request.requestHeaders.test2).toBe('2')
+      expect(request.requestHeaders.test3).toBe('3')
+    })
+  })
+
+  test('should add a response interceptor', done => {
+    let response: AxiosResponse
+    const instance = axios.create()
+
+    instance.interceptors.response.use(data => {
+      data.data = data.data + ' - modified by interceptor'
+      return data
+    })
+
+    instance('/foo').then(data => {
+      response = data
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK'
+      })
+
+      setTimeout(() => {
+        expect(response.data).toBe('OK - modified by interceptor')
+        done()
+      }, 100)
+    })
+  })
+
+  test('should add a response interceptor that returns a new data object', done => {
+    let response: AxiosResponse
+    const instance = axios.create()
+
+    instance.interceptors.response.use(() => {
+      return {
+        data: 'stuff',
+        headers: null,
+        status: 500,
+        statusText: 'ERR',
+        request: null,
+        config: {}
+      }
+    })
+
+    instance('/foo').then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK'
+      })
+
+      setTimeout(() => {
+        expect(response.data).toBe('stuff')
+        expect(response.headers).toBeNull()
+        expect(response.status).toBe(500)
+        expect(response.statusText).toBe('ERR')
+        expect(response.request).toBeNull()
+        expect(response.config).toEqual({})
+        done()
+      }, 100)
+    })
+  })
 })
