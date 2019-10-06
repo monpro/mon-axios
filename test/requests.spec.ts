34413ee3c75581654b5b34acde1fbc69627917ad
monpro <monproshen@gmail.com>
2019-10-06 11:02:12 +1100

    finish the unit test for request module

--- test/requests.spec.ts
+++ test/requests.spec.ts
@@ -186,4 +186,84 @@ describe('requests', () => {
       }, 100)
     })
   })
+
+  test('should return JSON when rejecting', done => {
+    let response: AxiosResponse
+
+    axios('/api/account/signup', {
+      auth: {
+        username: '',
+        password: ''
+      },
+      method: 'post',
+      headers: {
+        Accept: 'application/json'
+      }
+    }).catch(error => {
+      response = error.response
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 400,
+        statusText: 'Bad Request',
+        responseText: '{"error": "cannot find url", "code": 400}'
+      })
+
+      setTimeout(() => {
+        expect(typeof response.data).toBe('object')
+        expect(response.data.error).toBe('cannot find url')
+        expect(response.data.code).toBe(400)
+        done()
+      }, 100)
+    })
+  })
+
+  test('should return correct response', done => {
+    let response: AxiosResponse
+
+    axios.post('/foo').then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        statusText: 'OK',
+        responseText: '{"a": "1"}',
+        responseHeaders: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      setTimeout(() => {
+        expect(response.data.a).toBe('1')
+        expect(response.status).toBe(200)
+        expect(response.statusText).toBe('OK')
+        expect(response.headers['content-type']).toBe('application/json')
+        done()
+      }, 100)
+    })
+  })
+
+  test('should allow overriding Content-Type header case-insensitive', () => {
+    let response: AxiosResponse
+
+    axios
+      .post(
+        '/foo',
+        { prop: 'value' },
+        {
+          headers: {
+            'content-type': 'application/json'
+          }
+        }
+      )
+      .then(res => {
+        response = res
+      })
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['Content-Type']).toBe('application/json')
+    })
+  })
 })
