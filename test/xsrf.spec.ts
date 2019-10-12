790db63639e4720d60e5e5445e67c8d655bf233b
monpro <monproshen@gmail.com>
2019-10-12 22:46:57 +1100

    add xsrf test

--- test/xsrf.spec.ts
+++ test/xsrf.spec.ts
@@ -13,4 +13,52 @@ describe('xsrf', () => {
       '=;expires=' +
       new Date(Date.now() - 86400000).toUTCString()
   })
+
+  test('should not set xsrf header if cookie is null', () => {
+    axios('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(
+        request.requestHeaders[axios.defaults.xsrfHeaderName!]
+      ).toBeUndefined()
+    })
+  })
+
+  test('should set xsrf header if cookie is set', () => {
+    document.cookie = axios.defaults.xsrfCookieName + '=12345'
+
+    axios('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders[axios.defaults.xsrfHeaderName!]).toBe(
+        '12345'
+      )
+    })
+  })
+
+  test('should not set xsrf header for cross origin', () => {
+    document.cookie = axios.defaults.xsrfCookieName + '=12345'
+
+    axios('http://example.com/')
+
+    return getAjaxRequest().then(request => {
+      expect(
+        request.requestHeaders[axios.defaults.xsrfHeaderName!]
+      ).toBeUndefined()
+    })
+  })
+
+  test('should set xsrf header for cross origin when using withCredentials', () => {
+    document.cookie = axios.defaults.xsrfCookieName + '=12345'
+
+    axios('http://example.com/', {
+      withCredentials: true
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders[axios.defaults.xsrfHeaderName!]).toBe(
+        '12345'
+      )
+    })
+  })
 })
