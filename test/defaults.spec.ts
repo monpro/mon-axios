f71eb95eb52511730da49845a2e3f04ed1ecf80d
monpro <monproshen@gmail.com>
2019-10-12 18:05:25 +1100

    add test case of transformer

--- test/defaults.spec.ts
+++ test/defaults.spec.ts
@@ -10,4 +10,31 @@ describe('defaults', () => {
   afterEach(() => {
     jasmine.Ajax.uninstall()
   })
+
+  test('should transform request json', () => {
+    expect(
+      (axios.defaults.transformRequest as AxiosTransformer[])[0]({ test: 'a' })
+    ).toBe('{"test":"a"}')
+  })
+
+  test('should not transform request json', () => {
+    expect(
+      (axios.defaults.transformRequest as AxiosTransformer[])[0]('a:b')
+    ).toBe('a:b')
+  })
+
+  test('should transform response json', () => {
+    const data = (axios.defaults.transformResponse as AxiosTransformer[])[0](
+      '{"a":"b"}'
+    )
+
+    expect(typeof data).toBe('object')
+    expect(data.a).toBe('b')
+  })
+
+  test('should do nothing to response string', () => {
+    expect(
+      (axios.defaults.transformResponse as AxiosTransformer[])[0]('a=b')
+    ).toBe('a=b')
+  })
 })
