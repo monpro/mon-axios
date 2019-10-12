1b60fa67cba1d255e835cb425cb9e73639bdff53
monpro <monproshen@gmail.com>
2019-10-12 22:25:25 +1100

    add unit test for transform params

--- test/transform.spec.ts
+++ test/transform.spec.ts
@@ -9,4 +9,69 @@ describe('transform', () => {
   afterEach(() => {
     jasmine.Ajax.uninstall()
   })
+  test('should transform JSON to string', () => {
+    const data = {
+      foo: 'bar'
+    }
+
+    axios.post('/foo', data)
+
+    return getAjaxRequest().then(request => {
+      expect(request.params).toBe('{"foo":"bar"}')
+    })
+  })
+
+  test('should transform string to JSON', done => {
+    let response: AxiosResponse
+
+    axios('/foo').then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        responseText: '{"foo": "bar"}'
+      })
+
+      setTimeout(() => {
+        expect(typeof response.data).toBe('object')
+        expect(response.data.foo).toBe('bar')
+        done()
+      }, 100)
+    })
+  })
+
+  test('should override default transform', () => {
+    const data = {
+      foo: 'bar'
+    }
+
+    axios.post('/foo', data, {
+      transformRequest(data) {
+        return data
+      }
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.params).toEqual({ foo: 'bar' })
+    })
+  })
+
+  test('should allow an Array of transformers', () => {
+    const data = {
+      foo: 'bar'
+    }
+
+    axios.post('/foo', data, {
+      transformRequest: (axios.defaults
+        .transformRequest as AxiosTransformer[]).concat(function(data) {
+        return data.replace('bar', 'baz')
+      })
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.params).toBe('{"foo":"baz"}')
+    })
+  })
 })
