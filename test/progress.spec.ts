bec1d396fe26cdd4e081f6e6fdffc49a1c52bad9
monpro <monproshen@gmail.com>
2019-10-13 11:11:07 +1100

    init progress test

---
+++ test/progress.spec.ts
@@ -0,0 +1,12 @@
+import axios from '../src/index'
+import { getAjaxRequest } from './helper'
+
+describe('progress', () => {
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+})
