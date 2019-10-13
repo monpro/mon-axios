5eb82cb2e18859de2180d2aea5559738b49135ef
monpro <monproshen@gmail.com>
2019-10-13 11:13:54 +1100

    finish download test

--- test/progress.spec.ts
+++ test/progress.spec.ts
@@ -9,4 +9,28 @@ describe('progress', () => {
   afterEach(() => {
     jasmine.Ajax.uninstall()
   })
+
+  test('should add a download progress handler', () => {
+    const progressSpy = jest.fn()
+
+    axios('/foo', { onDownloadProgress: progressSpy })
+
+    return getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        responseText: '{"foo": "bar"}'
+      })
+      expect(progressSpy).toHaveBeenCalled()
+    })
+  })
+
+  test('should add a upload progress handler', () => {
+    const progressSpy = jest.fn()
+
+    axios('/foo', { onUploadProgress: progressSpy })
+
+    return getAjaxRequest().then(request => {
+      //   expect(progressSpy).toHaveBeenCalled()
+    })
+  })
 })
